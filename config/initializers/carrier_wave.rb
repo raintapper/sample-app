815db7cd1ed26e8929e3a45f1f04f37c61c1a3f9
raintapper <chew.barry@gmail.com>
2016-03-24 15:38:07 +0000

    Redid FOG GEM and AWS

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,8 +3,8 @@ if	Rails.env.production?
   config.fog_credentials	=	{
     #	Configuration	for	Amazon	S3
     :provider								=>	'AWS',
-    :aws_access_key_id			=>	ENV['AKIAIMETUHIJCOQVEPHA'],
-    :aws_secret_access_key	=>	ENV['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']				}
-    config.fog_directory    =		ENV['raintapper']
+    :aws_access_key_id			=>	ENV['S3_ACCESS_KE'],
+    :aws_secret_access_key	=>	ENV['S3_SECRET_KEY']				}
+    config.fog_directory    =		ENV['S3_BUCKET']
   end
 end
